d131502f53f2fda84d82a22010d9678f2c054bd8
Mislav Matijević <mislav.mim@gmail.com>
2024-08-08 12:23:39 +0200

    Enable evaluation of CPP solution. Implemented comparison of expected outputs and actual outputs per tests. Better solution would certainly be running tests with dynamic values, but my holidays are running short, so this'll do.

--- internal/handlers/tasks/execute_task.go
+++ internal/handlers/tasks/execute_task.go
@@ -21,12 +21,23 @@ type taskExecutionRequest = struct {
 	SolutionCode string `json:"solution_code"`
 }
 
+type testDataMismatch = struct {
+	TestInput   string `json:"test_input"`
+	WrongOutput string `json:"wrong_output"`
+}
+
 type taskExecutionResponse = struct {
-	Success bool   `json:"success"`
-	Message string `json:"message"`
+	Success      bool              `json:"success"`
+	Message      string            `json:"message"`
+	ReasonFailed *testDataMismatch `json:"reason,omitempty"`
 }
 
 func ExecuteTask(w http.ResponseWriter, r *http.Request) {
+	requestBody, failedToGetRequestBody := getRequestBody(r, w)
+	if failedToGetRequestBody {
+		return
+	}
+
 	var originalParamId = chi.URLParam(r, "taskId")
 	taskId, err := strconv.Atoi(originalParamId)
 	if err != nil {
@@ -34,126 +45,65 @@ func ExecuteTask(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	inputs, expectedOutputs, err := getTestsForTask(taskId)
-	if err != nil || expectedOutputs == "" {
+	var tests []database.Test = database.GetTestsForTask(taskId)
+	if len(tests) == 0 {
 		res := taskExecutionResponse{Success: false, Message: "Can't test this task."}
 		w.Header().Add("content-type", "application/json")
 		json.NewEncoder(w).Encode(res)
 		return
 	}
 
-	requestBody, failedToGetRequestBody := getRequestBody(r, w)
-	if failedToGetRequestBody {
-		return
-	}
+	var res taskExecutionResponse
 
-	cppFile, err := storeTempFiles(requestBody.SolutionCode, inputs)
-	var tempDirPath = filepath.Dir(cppFile.Name())
-	if err != nil {
-		api.InternalErrorHandler(w)
+	for _, test := range tests {
+		testInput := test.Input
+
+		cppFile, err := storeTempFiles(requestBody.SolutionCode, testInput)
+		var tempDirPath = filepath.Dir(cppFile.Name())
+		if err != nil {
+			api.InternalErrorHandler(w)
+			os.RemoveAll(tempDirPath)
+			return
+		}
+
+		err = runFileInIsolatedDockerContainer(cppFile)
+		if err != nil {
+			api.InternalErrorHandler(w)
+			os.RemoveAll(tempDirPath)
+			return
+		}
+
+		actualOutputs, err := getOutputs(tempDirPath)
+		if err != nil {
+			api.InternalErrorHandler(w)
+			os.RemoveAll(tempDirPath)
+			return
+		}
+		// TODO read artefacts
+
 		os.RemoveAll(tempDirPath)
-		return
+
+		if actualOutputs != test.ExpectedOutput {
+			res = taskExecutionResponse{
+				Success: false,
+				Message: "Program did not output expected test data.",
+				ReasonFailed: &testDataMismatch{
+					TestInput:   testInput,
+					WrongOutput: actualOutputs,
+				},
+			}
+			sendResponse(w, res)
+			return
+		}
 	}
 
-	err = runFileInIsolatedDockerContainer(cppFile)
-	if err != nil {
-		api.InternalErrorHandler(w)
-		os.RemoveAll(tempDirPath)
-		return
-	}
+	res = taskExecutionResponse{Success: true, Message: "Test data matches output!", ReasonFailed: nil}
+	sendResponse(w, res)
+}
 
-	res := taskExecutionResponse{Success: true, Message: "test"}
-
+func sendResponse(w http.ResponseWriter, res taskExecutionResponse) {
 	w.Header().Add("content-type", "application/json")
 	json.NewEncoder(w).Encode(res)
-}
-
-func getTestsForTask(taskId int) (string, string, error) {
-	var tests []database.Test = database.GetTestsForTask(taskId)
-
-	var inputs strings.Builder
-	var outputs strings.Builder
-	for _, test := range tests {
-		inputs.WriteString(test.Input)
-		inputs.WriteRune('\n')
-		outputs.WriteString(test.ExpectedOutput)
-		outputs.WriteRune('\n')
-	}
-
-	return inputs.String(), outputs.String(), nil
-}
-
-func runFileInIsolatedDockerContainer(cppFile *os.File) error {
-	err := runDockerRunnerImage(cppFile.Name())
-
-	if err != nil {
-		if err.Error() == "image not built" {
-			log.Warn("Image wasn't built!")
-			buildErr := buildRunnerImage()
-			if buildErr != nil {
-				log.Errorf("Failed to build Docker container: %s", err)
-				return err
-			}
-
-			err = runDockerRunnerImage(cppFile.Name())
-		}
-
-		if err != nil {
-			log.Errorf("Failed to run Docker container: %v", err)
-			return err
-		}
-	}
-
-	return nil
-}
-
-func buildRunnerImage() error {
-	log.Info("Building task-runner image.")
-	dockerPath, err := exec.LookPath("docker")
-	if err != nil {
-		return err
-	}
-	cmd := exec.Command("bash", "-c", dockerPath+" build -t task-runner:latest -f ./task-runner.Dockerfile .")
-	return cmd.Run()
-}
-
-func runDockerRunnerImage(fullFilePath string) error {
-	log.Info("Running Docker image task-runner")
-
-	var sourceCodePath = filepath.Dir(fullFilePath)
-	var parentFolderName = filepath.Base(sourceCodePath)
-	var sourceFileName = filepath.Base(fullFilePath)
-
-	dockerPath, err := exec.LookPath("docker")
-	if err != nil {
-		return err
-	}
-
-	var volumeName = os.Getenv("TASKS_VOLUME_NAME")
-
-	var dockerRunArguments = fmt.Sprintf(
-		"%s run --rm "+
-			"-v %s:/var/temp_tasks/ "+
-			"--memory 30m --cpus 0.15 "+
-			"--security-opt no-new-privileges --network none "+
-			"-e SOURCE_CODE_FOLDER=%s "+
-			"-e SOURCE_FILE_NAME=%s "+
-			"task-runner:latest", dockerPath, volumeName, parentFolderName, sourceFileName,
-	)
-
-	cmd := exec.Command("bash", "-c", dockerRunArguments)
-
-	output, err := cmd.CombinedOutput()
-	var stringOutput = string(output)
-	if stringOutput != "" {
-		if strings.Contains(stringOutput, "Unable to find image 'task-runner:latest'") {
-			return errors.New("image not built")
-		} else {
-			log.Warningf("Suspicious output from task-runner container: %s", stringOutput)
-		}
-	}
-
-	return err
 }
 
 func getRequestBody(r *http.Request, w http.ResponseWriter) (*taskExecutionRequest, bool) {
@@ -212,4 +162,82 @@ func fillFileWithData(tempFile *os.File, inputs string) error {
 		log.Errorf("Failed to insert data in the temp file (%s)\n%s", tempFile.Name(), err)
 	}
 	return err
+}
+
+func runFileInIsolatedDockerContainer(cppFile *os.File) error {
+	err := runDockerRunnerImage(cppFile.Name())
+
+	if err != nil {
+		if err.Error() == "image not built" {
+			log.Warn("Image wasn't built!")
+			buildErr := buildRunnerImage()
+			if buildErr != nil {
+				log.Errorf("Failed to build Docker container: %s", err)
+				return err
+			}
+
+			err = runDockerRunnerImage(cppFile.Name())
+		}
+
+		if err != nil {
+			log.Errorf("Failed to run Docker container: %v", err)
+			return err
+		}
+	}
+
+	return nil
+}
+
+func buildRunnerImage() error {
+	log.Info("Building task-runner image.")
+	dockerPath, err := exec.LookPath("docker")
+	if err != nil {
+		return err
+	}
+	cmd := exec.Command("bash", "-c", dockerPath+" build -t task-runner:latest -f ./task-runner.Dockerfile .")
+	return cmd.Run()
+}
+
+func runDockerRunnerImage(fullFilePath string) error {
+	var sourceCodePath = filepath.Dir(fullFilePath)
+	var parentFolderName = filepath.Base(sourceCodePath)
+	var sourceFileName = filepath.Base(fullFilePath)
+
+	dockerPath, err := exec.LookPath("docker")
+	if err != nil {
+		return err
+	}
+
+	var volumeName = os.Getenv("TASKS_VOLUME_NAME")
+
+	var dockerRunArguments = fmt.Sprintf(
+		"%s run --rm "+
+			"-v %s:/var/temp_tasks/ "+
+			"--memory 30m --cpus 0.15 "+
+			"--security-opt no-new-privileges --network none "+
+			"-e SOURCE_CODE_FOLDER=%s "+
+			"-e SOURCE_FILE_NAME=%s "+
+			"task-runner:latest", dockerPath, volumeName, parentFolderName, sourceFileName,
+	)
+
+	cmd := exec.Command("bash", "-c", dockerRunArguments)
+
+	output, err := cmd.CombinedOutput()
+	var stringOutput = string(output)
+	if stringOutput != "" {
+		if strings.Contains(stringOutput, "Unable to find image 'task-runner:latest'") {
+			return errors.New("image not built")
+		} else {
+			log.Warningf("Suspicious output from task-runner container: %s", stringOutput)
+		}
+	}
+
+	return err
+}
+
+func getOutputs(tempDirPath string) (string, error) {
+	bytes, err := os.ReadFile(filepath.Join(tempDirPath, "stdout.txt"))
+	var stringOutput = string(bytes)
+	stringOutput = strings.TrimFunc(stringOutput, func(r rune) bool { return r == '\n' || r == ' ' })
+	return stringOutput, err
 }
