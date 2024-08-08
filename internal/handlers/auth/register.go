eab3a92be9f8e9de2861e4dc9d8a8853faad9226
Mislav Matijević <mislav.mim@gmail.com>
2024-08-08 22:38:44 +0200

    Validate registration data.

--- internal/handlers/auth/register.go
+++ internal/handlers/auth/register.go
@@ -3,6 +3,7 @@ package auth
 import (
 	"encoding/json"
 	"net/http"
+	"regexp"
 	"strings"
 	"time"
 
@@ -30,6 +31,12 @@ func RegisterUser(w http.ResponseWriter, r *http.Request) {
 	}
 
 	username, email := userReqBody.Username, userReqBody.Email
+	var infoIsValid bool = checkIfUserInfoValid(username, email, userReqBody.Password)
+	if !infoIsValid {
+		api.RequestErrorHandlerCustomMsg(w, "User information is not valid for registration.")
+		return
+	}
+
 	hashPassword, err := getHashPassword(userReqBody.Password)
 	if err != nil {
 		api.InternalErrorHandler(w, err)
@@ -60,6 +67,19 @@ func getHashPassword(password string) (string, error) {
 		return "", err
 	}
 	return string(hash), nil
+}
+
+func checkIfUserInfoValid(username, email, password string) bool {
+	var nonEmptyStrings = strings.Trim(username, " ") != "" && strings.Trim(email, " ") != ""
+	var isEmailValid = isEmailValid(email)
+	var usernameDoesNotContainAt = !strings.Contains(username, "@")
+	var passwordAtLeast8Chars = len(password) >= 8
+	return nonEmptyStrings && isEmailValid && usernameDoesNotContainAt && passwordAtLeast8Chars
+}
+
+func isEmailValid(e string) bool {
+	emailRegex := regexp.MustCompile(`^[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,4}$`)
+	return emailRegex.MatchString(e)
 }
 
 func createUser(username, email, hashPassword string) database.User {
