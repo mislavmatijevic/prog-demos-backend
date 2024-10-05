aff88663171a000d3869e6713e688443b53e7914
Mislav Matijević <mislav.mim@gmail.com>
2024-10-05 18:16:33 +0200

    Minor method name improvement.

--- internal/handlers/api/error_handler.go
+++ internal/handlers/api/error_handler.go
@@ -15,7 +15,7 @@ func (e errorResponse) Error() string {
 	return e.Message
 }
 
-func writerError(w http.ResponseWriter, message string, code int) {
+func writeError(w http.ResponseWriter, message string, code int) {
 	resp := errorResponse{
 		Success: false,
 		Message: message,
@@ -27,34 +27,34 @@ func writerError(w http.ResponseWriter, message string, code int) {
 var (
 	RequestErrorHandlerGenericMsg = func(w http.ResponseWriter, err error) {
 		log.Error(err)
-		writerError(w, "Request invalid.", http.StatusBadRequest)
+		writeError(w, "Request invalid.", http.StatusBadRequest)
 	}
 	RequestErrorHandlerCustomMsg = func(w http.ResponseWriter, errorMessage string) {
-		writerError(w, errorMessage, http.StatusBadRequest)
+		writeError(w, errorMessage, http.StatusBadRequest)
 	}
 	InternalErrorHandlerGenericMsg = func(w http.ResponseWriter, err error) {
 		log.Error(err)
-		writerError(w, "An Unexpected Error Occurred.", http.StatusInternalServerError)
+		writeError(w, "An Unexpected Error Occurred.", http.StatusInternalServerError)
 	}
 	InternalErrorHandlerCustomMsg = func(w http.ResponseWriter, errorMessage string) {
-		writerError(w, errorMessage, http.StatusInternalServerError)
+		writeError(w, errorMessage, http.StatusInternalServerError)
 	}
 	AuthorizationMissingGenericMsg = func(w http.ResponseWriter) {
-		writerError(w, "Valid authorization header missing.", http.StatusUnauthorized)
+		writeError(w, "Valid authorization header missing.", http.StatusUnauthorized)
 	}
 	AuthorizationExpiredGenericMsg = func(w http.ResponseWriter) {
-		writerError(w, "Authorization token expired.", http.StatusForbidden)
+		writeError(w, "Authorization token expired.", http.StatusForbidden)
 	}
 	AuthorizationInvalidGenericMsg = func(w http.ResponseWriter) {
-		writerError(w, "Authorization token is invalid.", http.StatusForbidden)
+		writeError(w, "Authorization token is invalid.", http.StatusForbidden)
 	}
 	AuthorizationInvalidCustomMsg = func(w http.ResponseWriter, errorMessage string) {
-		writerError(w, errorMessage, http.StatusForbidden)
+		writeError(w, errorMessage, http.StatusForbidden)
 	}
 	TooEarlyErrorHandlerCustomMsg = func(w http.ResponseWriter, errorMessage string) {
-		writerError(w, errorMessage, http.StatusTooEarly)
+		writeError(w, errorMessage, http.StatusTooEarly)
 	}
 	NotFoundHandlerCustomMsg = func(w http.ResponseWriter, errorMessage string) {
-		writerError(w, errorMessage, http.StatusNotFound)
+		writeError(w, errorMessage, http.StatusNotFound)
 	}
 )
