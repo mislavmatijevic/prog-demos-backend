61ab1add421aaee0e325198634e14ecc71fc6e00
Mislav Matijević <mislav.mim@gmail.com>
2024-10-12 02:23:01 +0200

    Introduce throttling of task execution requests.

--- internal/handlers/tasks/handler.go
+++ internal/handlers/tasks/handler.go
@@ -1,7 +1,10 @@
 package tasks
 
 import (
+	"time"
+
 	"github.com/go-chi/chi/v5"
+	chimiddle "github.com/go-chi/chi/v5/middleware"
 	"github.com/mislavmatijevic/prog-demos-backend/internal/authentication"
 )
 
@@ -12,6 +15,7 @@ func HandleTasks(r *chi.Mux) {
 
 		router.Group(func(protectedRouter chi.Router) {
 			protectedRouter.Use(authentication.RequireAccessToken)
+			protectedRouter.Use(chimiddle.ThrottleBacklog(10, 20, 30*time.Second))
 
 			protectedRouter.Post("/{taskId}/run", executeTask)
 		})
